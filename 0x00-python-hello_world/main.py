ae44864a41f10835bc6bfdb96cb525b7e625d478
Salman ElHussini <hussainyred@gmail.com>
2023-11-06 17:58:54 +0200

    task 2 build a puzzle - late

--- 0x00-python-hello_world/main.py
+++ 0x00-python-hello_world/main.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+x = 3
+steps = 0
 
-print("hello world")
+while x < 70:
+    x += 3
+    steps += 1
+    print(x)
+
+print("It takes", steps, "steps to reach 70 starting from 3.")
+
