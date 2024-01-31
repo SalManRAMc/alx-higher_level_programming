1fad6e3215d36bd0ae18de44d0b949eb50136c00
SalmanRAMc <hussainyred@gmail.com>
2024-01-31 18:53:21 +0200

    +1

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,11 +2,9 @@
 
 def uppercase(str):
     i = 0
-    while (str[i] != "\0"):
-        {
-            if str[i] == " ":
-                print(" ")
-            else:
-                print("{:c}".format(chr(ord(str[i]) - 32)))
-            i++
-        }
+    while i < len(str):
+        if str[i] == " ":
+            print(" ")
+        else:
+            print("{:c}".format(chr(ord(str[i]) - 32)))
+        i += 1
