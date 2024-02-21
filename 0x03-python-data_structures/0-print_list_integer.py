df3181991490fd11637b8469e41bfb9bcbb94ddc
SalmanRAMc <hussainyred@gmail.com>
2024-02-21 11:15:46 +0200

    pol

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        print("{:d}".format(my_list[i]), "\n") 
-        #remember that in order to print something as a specific data type
-        #you have to specify the data type inside the curly braces
+        print("{:d}".format(my_list[i]), "\n")
+# remember that in order to print something as a specific data type
+# you have to specify the data type inside the curly braces
