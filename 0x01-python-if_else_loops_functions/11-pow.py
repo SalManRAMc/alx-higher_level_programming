521cf8d399e16df97d3d2063efaaef9e87eefeab
SalmanRAMc <hussainyred@gmail.com>
2024-05-04 11:13:39 +0300

    automated alias message.. thanks bash

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+  This is a power function
+"""
+def pow(a, b):
+    result = 1
+    if b < 0:
+        return (1 / pow(a, -b)) #Handle negative powers
+    elif b == 0:
+        return (1)
+    else:    
+        for i in range(b):
+            result *= a
+        return result
