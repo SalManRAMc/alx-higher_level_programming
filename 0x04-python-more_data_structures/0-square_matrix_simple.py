11ce0fb4851b0015f5dc7735eca93c5b4252b899
SalmanRAMc <hussainyred@gmail.com>
2024-05-06 16:19:53 +0300

    automated alias message.. thanks bash

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda submatrix: list(map(lambda element: element**2, submatrix)), matrix))
+    """
+        mat: matrix
+        smat: submatrix
+        e: element
+        The function takes a matrix and returns a new squared matrix
+        inside the inner lambda constructor
+    """
+    return list(map(lambda smat: list(map(lambda e: e**2, smat)), mat))
