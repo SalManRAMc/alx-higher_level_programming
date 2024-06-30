248cd5f4b7287ef0304af627c5412e00c71f544c
SalmanRAMc <hussainyred@gmail.com>
2024-06-30 15:05:34 +0300

    automated alias message.. thanks bash

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    realnumbers = 0
     i = 0
     try:
-        while i is not x:
+        while i < x:
             print(my_list[i], end="")
-            realnumbers += 1
             i += 1
         print("\n")
-        return (realnumbers)
     except (IndexError):
         print("")
+    return (i)
