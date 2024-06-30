d1904015a9ba22226ec02943428bd2420d86c0d4
SalmanRAMc <hussainyred@gmail.com>
2024-06-30 14:43:45 +0300

    automated alias message.. thanks bash

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,5 +6,5 @@ def safe_print_list(my_list=[], x=0):
         while i < x:
             print(my_list[i], end="")
             realnumbers += 1
-    except LimitError:
-        print("invalid X")
+    except:
+        print("An Unknown exception has been raised")
