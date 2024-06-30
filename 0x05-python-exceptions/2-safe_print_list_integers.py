9306a550867b91fd2e31a70f73fe394ca2fe08f9
SalmanRAMc <hussainyred@gmail.com>
2024-06-30 16:08:08 +0300

    automated alias message.. thanks bash

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,12 +5,13 @@ def safe_print_list_integers(my_list=[], x=0):
     realnumbers = 0
     while i != x:
         try:
-                print("{:d}".format(my_list[i]), end="")
-                realnumbers += 1
+            print("{:d}".format(my_list[i]), end="")
+            realnumbers += 1
         except (TypeError, ValueError):
             pass
         finally:
             i += 1
     print()
     return (realnumbers)
+
 
