2240271d58c716a2ed6f0222cca098e12868d290
SalmanRAMc <hussainyred@gmail.com>
2024-06-30 14:54:21 +0300

    automated alias message.. thanks bash

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,13 @@
 
 def safe_print_list(my_list=[], x=0):
     realnumbers = 0
+    i = 0
     try:
-        while i < x:
+        while i is not x:
             print(my_list[i], end="")
             realnumbers += 1
+            i += 1
         print("")
         return (realnumbers)
-    except (IndexError, NameError):
-        print("An Unknown exception has been raised")
+    except (IndexError):
+        print("You are trying to access an unreachable index")
