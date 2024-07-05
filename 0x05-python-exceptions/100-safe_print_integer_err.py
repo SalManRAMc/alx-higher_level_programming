75030a98391faa8369594cb1044fe43b72438325
SalmanRAMc <hussainyred@gmail.com>
2024-07-05 16:18:26 +0300

    automated alias message.. thanks bash

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import sys
 
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except:
-        
+    except (TypeError, ValueError) as err:
+        print("Exception:", err, file=sys.stderr)
+        return False
+
