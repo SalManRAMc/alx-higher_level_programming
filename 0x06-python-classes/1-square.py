22d8997365e919c427bd3c381de8a8d85a98f274
SalmanRAMc <hussainyred@gmail.com>
2024-07-06 16:18:14 +0300

    automated alias message.. thanks bash

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is still the module's documentation
+Still working on the square class
+add a little more
+"""
+
+
+class square:
+    """This is the object documentation"""
+    __size = 0
+
+    def __init__(self, size):
+        self.__size = size
